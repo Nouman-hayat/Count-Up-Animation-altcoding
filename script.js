b6897861aaac8f07825f781f94e08edc8e5e4e67
Nouman hayat <75477552+Nouman-hayat@users.noreply.github.com>
2023-06-05 14:41:47 +0500

    p1

---
+++ script.js
@@ -0,0 +1,15 @@
+let interval = 4000;
+let Displayvalue = document.querySelectorAll(".num");
+
+Displayvalue.forEach((valueDisplay) => {
+	let startValue = 0;
+	let endValue = parseInt(valueDisplay.getAttribute("data-val"));
+	let duration = Math.floor(interval / endValue);
+	let counter = setInterval(function () {
+		startValue += 1;
+		valueDisplay.textContent = startValue;
+		if (startValue == endValue) {
+			clearInterval(counter);
+		}
+	}, duration);
+});
